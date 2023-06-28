82fc093bdd9082bb3436ef7d572848a2353ab268
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-06-28 08:17:32 +0800

    Integrated Slack API

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Homepage_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20230628-061625/backstop_default_Homepage_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230628-080747/backstop_default_Homepage_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_Homepage_0_document_0_desktop.png",
         "label": "Homepage",
@@ -21,42 +21,16 @@ report({
           },
           "rawMisMatchPercentage": 29.99485485268631,
           "misMatchPercentage": "29.99",
-          "analysisTime": 471
+          "analysisTime": 461
         },
-        "diffImage": "../bitmaps_test/20230628-061625/failed_diff_backstop_default_Homepage_0_document_0_desktop.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/backstop_default_JOIN-PLUS_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20230628-061625/backstop_default_JOIN-PLUS_0_document_0_desktop.png",
-        "selector": "document",
-        "fileName": "backstop_default_JOIN-PLUS_0_document_0_desktop.png",
-        "label": "JOIN-PLUS",
-        "misMatchThreshold": 0.1,
-        "url": "https://dev-greyscalegorilla.pantheonsite.io/join-plus",
-        "referenceUrl": "https://greyscalegorilla.com/join-plus",
-        "expect": 0,
-        "viewportLabel": "desktop",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": 189
-          },
-          "rawMisMatchPercentage": 26.03754261758976,
-          "misMatchPercentage": "26.04",
-          "analysisTime": 398
-        },
-        "diffImage": "../bitmaps_test/20230628-061625/failed_diff_backstop_default_JOIN-PLUS_0_document_0_desktop.png"
+        "diffImage": "../bitmaps_test/20230628-080747/failed_diff_backstop_default_Homepage_0_document_0_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BLOG_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20230628-061625/backstop_default_BLOG_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230628-080747/backstop_default_BLOG_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_BLOG_0_document_0_desktop.png",
         "label": "BLOG",
@@ -73,9 +47,61 @@ report({
           },
           "rawMisMatchPercentage": 41.04946275979434,
           "misMatchPercentage": "41.05",
-          "analysisTime": 250
+          "analysisTime": 247
         },
-        "diffImage": "../bitmaps_test/20230628-061625/failed_diff_backstop_default_BLOG_0_document_0_desktop.png"
+        "diffImage": "../bitmaps_test/20230628-080747/failed_diff_backstop_default_BLOG_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_JOIN-PLUS_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230628-080747/backstop_default_JOIN-PLUS_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_JOIN-PLUS_0_document_0_desktop.png",
+        "label": "JOIN-PLUS",
+        "misMatchThreshold": 0.1,
+        "url": "https://dev-greyscalegorilla.pantheonsite.io/join-plus",
+        "referenceUrl": "https://greyscalegorilla.com/join-plus",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 189
+          },
+          "rawMisMatchPercentage": 26.013697061415712,
+          "misMatchPercentage": "26.01",
+          "analysisTime": 393
+        },
+        "diffImage": "../bitmaps_test/20230628-080747/failed_diff_backstop_default_JOIN-PLUS_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_TUTORIALS_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230628-080747/backstop_default_TUTORIALS_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_TUTORIALS_0_document_0_desktop.png",
+        "label": "TUTORIALS",
+        "misMatchThreshold": 0.1,
+        "url": "https://dev-greyscalegorilla.pantheonsite.io/tutorials",
+        "referenceUrl": "https://greyscalegorilla.com/tutorials",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -811
+          },
+          "rawMisMatchPercentage": 44.74092033844189,
+          "misMatchPercentage": "44.74",
+          "analysisTime": 134
+        },
+        "diffImage": "../bitmaps_test/20230628-080747/failed_diff_backstop_default_TUTORIALS_0_document_0_desktop.png"
       },
       "status": "fail"
     }
--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const { exec } = require('child_process');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-
+const { WebClient } = require('@slack/web-api');
+// // Initialize a single instance for the whole app
 const app = express();
+//
+// Read a token from the environment variables
+const token = 'xoxb-5507137915921-5487870130582-pngv30mRLNmzPugyuozZHNCL';
+
+// Initialize
+const web = new WebClient(token);
+
+const conversationId = 'C05E3UJQX2B';
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the BackstopJS report directory
+app.use('/backstop-data', express.static(path.join(__dirname, '/backstop_data')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -75,6 +87,20 @@ app.post('/compare', (req, res) => {
 
     exec(`backstop reference`, () => {
         exec(`backstop test`, () => {
+
+            (async () => {
+
+                // Post a message to the channel, and await the result.
+                // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+                const result = await web.chat.postMessage({
+                    text: 'https://vrt.kbsan.com/backstop-data/html_report/',
+                    channel: conversationId,
+                });
+
+                // The result contains an identifier for the message, `ts`.
+                console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
+            })();
+
             return res.redirect('/');
         });
     });
