f2c3c1193bc82052e175d8956ee58cec5cca28bc
Kim Benedict Santiago <kimramirezsantiago@gmail.com>
2023-06-28 08:54:05 +0800

    Use port 8080

--- server.js
+++ server.js
@@ -106,4 +106,4 @@ app.post('/compare', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server running on https://vrt.kbsan.com'));
+app.listen(8080, () => console.log('Server running on https://vrt.kbsan.com:8080'));
